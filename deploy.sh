6e0aff64f716c936c6de2ead1e4c61da57596027
Lee Artem <fcknawsm@ya.ru>
2022-12-20 13:48:52 +0300

    fixes

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,5 @@
-docker compose up -d && docker compose run --rm composer i && docker compose run --rm artisan migrate:refresh && cd src && npm i && npm run dev
+docker compose up -d \
+&& docker compose run --rm composer i \
+&& docker compose run --rm artisan migrate:refresh \
+&& docker compose run --rm npm i \
+&& docker compose run --rm --service-ports npm run dev
--- src/vite.config.js
+++ src/vite.config.js
@@ -12,4 +12,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+        open: false,
+    }
 });
